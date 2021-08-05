fc471f12acc0bbc9583bd887934189b18db4a4ca
gshnlgnv <gshnlgnv@gmail.com>
2021-08-05 12:23:06 +0300

    'init'

---
+++ my-api/src/product/product.model.ts
@@ -0,0 +1,13 @@
+export class ProductModel {
+  image: string;
+  title: string;
+  price: number;
+  oldPrice: number;
+  credit: number;
+  calculatedRating: number;
+  description: string;
+  advantages: string;
+  disadvantages: string;
+  categories: string[];
+  tags: string;
+}
